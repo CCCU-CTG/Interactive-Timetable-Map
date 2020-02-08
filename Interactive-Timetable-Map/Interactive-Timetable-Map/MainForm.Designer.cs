3950c4f3a4fd5c1036c600aba0b5eaf84b35e562
Gentian Gashi <genti_gashi8@hotmail.com>
2020-02-08 15:07:52 +0000

    Interactive-Timetable-Map
    Added UI framework

--- Interactive-Timetable-Map/Interactive-Timetable-Map/MainForm.Designer.cs
+++ Interactive-Timetable-Map/Interactive-Timetable-Map/MainForm.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.timeTableGroupBoxUI = new System.Windows.Forms.GroupBox();
+            this.userGroupBoxUI = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // timeTableGroupBoxUI
+            // 
+            this.timeTableGroupBoxUI.Location = new System.Drawing.Point(12, 12);
+            this.timeTableGroupBoxUI.Name = "timeTableGroupBoxUI";
+            this.timeTableGroupBoxUI.Size = new System.Drawing.Size(593, 426);
+            this.timeTableGroupBoxUI.TabIndex = 0;
+            this.timeTableGroupBoxUI.TabStop = false;
+            this.timeTableGroupBoxUI.Text = "Timetable";
+            // 
+            // userGroupBoxUI
+            // 
+            this.userGroupBoxUI.Location = new System.Drawing.Point(611, 12);
+            this.userGroupBoxUI.Name = "userGroupBoxUI";
+            this.userGroupBoxUI.Size = new System.Drawing.Size(177, 426);
+            this.userGroupBoxUI.TabIndex = 1;
+            this.userGroupBoxUI.TabStop = false;
+            this.userGroupBoxUI.Text = "User";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userGroupBoxUI);
+            this.Controls.Add(this.timeTableGroupBoxUI);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox timeTableGroupBoxUI;
+        private System.Windows.Forms.GroupBox userGroupBoxUI;
     }
 }
 
