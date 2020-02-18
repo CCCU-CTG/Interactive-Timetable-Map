e67c6a5b98c5012c5f20928c041a682cece84acb
Doktor404 <60747014+Doktor404@users.noreply.github.com>
2020-02-18 19:08:53 +0000

    Add files via upload
    Full working code for AEScryp login

--- Interactive-Timetable-Map/Interactive-Timetable-Map/Program.cs
+++ Interactive-Timetable-Map/Interactive-Timetable-Map/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Interactive_Timetable_Map
+namespace Login_System_Tut_ecnrypted
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Interactive_Timetable_Map
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Form1());
         }
     }
 }
