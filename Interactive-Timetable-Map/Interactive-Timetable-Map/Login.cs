a676c3d11a97d8dd964d09d0de0318bba0086483
Rukia <Connor@CONNOR-PC>
2020-02-19 06:32:35 +0000

    Changed project folder name to make Git Commits easier, merged RegForm and LoginForm, updated variable names for clarity, several minor bugfixes

--- Interactive-Timetable-Map/Interactive-Timetable-Map/Login.cs
+++ Interactive-Timetable-Map/Interactive-Timetable-Map/Login.cs
@@ -7,50 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using AesEncDec;
 using System.IO;
 
-namespace Login_System_Tut_ecnrypted
+namespace Interactive_Timetable_Map
 {
-    public partial class Form1 : Form
+    public partial class loginForm : Form
     {
-        public Form1()
+        bool loggedInTemp = false;
+
+        public loginForm()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void loginButton_Click(object sender, EventArgs e)
         {
-            RegForm rf = new RegForm();
-            rf.Show();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (usrTxt.Text.Length < 3 || passTxt.Text.Length < 5)
+            if (usernameTextBox.Text.Length < 3 || passwordTextBox.Text.Length < 5)
             {
                 MessageBox.Show("Username or Password is invaled or too short!");
             }
             else
             {
-                string dir = usrTxt.Text;
+                string dir = usernameTextBox.Text;
                 if (!Directory.Exists("data\\" + dir))
                     MessageBox.Show("User {0} was not found!", dir);
                 else
                 {
                     var sr = new StreamReader("data\\" + dir + "\\data.ls");
 
-                    string encusr = sr.ReadLine();
-                    string encpass = sr.ReadLine();
+                    string encryptedusername = sr.ReadLine();
+                    string encryptedpassword = sr.ReadLine();
                     sr.Close();
 
-                    string decusr = AesCryp.Decrypt(encusr);
-                    string decpass = AesCryp.Decrypt(encpass);
+                    string decusr = AesCryp.Decrypt(encryptedusername);
+                    string decpass = AesCryp.Decrypt(encryptedpassword);
 
-                    if (decusr == usrTxt.Text && decpass == passTxt.Text)
+                    if (decusr == usernameTextBox.Text && decpass == passwordTextBox.Text)
                     {
                         MessageBox.Show("Welcome {0} to the private area!", decusr);
+                        loggedInTemp = true;
+                        // Returns loggedInTemp to LoginCheck in MainForm
+                        Application.OpenForms.OfType<MainForm>().First().LoginCheck(loggedInTemp);
+                        this.Close();
                     }
                     else
                     {
@@ -58,6 +57,36 @@ namespace Login_System_Tut_ecnrypted
                     }
 
                 }
+            }
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            if (usernameTextBox.Text.Length < 3 || passwordTextBox.Text.Length < 5)
+            {
+                MessageBox.Show("Username or Password is invaled or too short!");
+            }
+            else
+            {
+                string dir = usernameTextBox.Text;
+                // Checks to see if the username already exists, if so, abort registration
+                if(Directory.Exists("data\\" + dir))
+                {
+                    MessageBox.Show("This Username Is Taken");
+                    return;
+                }
+                Directory.CreateDirectory("data\\" + dir);
+
+                var sw = new StreamWriter("data\\" + dir + "\\data.ls");
+
+                string encryptedusername = AesCryp.Encrypt(usernameTextBox.Text);
+                string encryptedpassword = AesCryp.Encrypt(passwordTextBox.Text);
+
+                sw.WriteLine(encryptedusername);
+                sw.WriteLine(encryptedpassword);
+                sw.Close();
+
+                MessageBox.Show("User {0} was successfully created.", usernameTextBox.Text);
             }
         }
     }
--- Interactive-Timetable-Map/Interactive-Timetable-Map/Program.cs
+++ Interactive-Timetable-Map/Interactive-Timetable-Map/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Login_System_Tut_ecnrypted
+namespace Interactive_Timetable_Map
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Login_System_Tut_ecnrypted
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
