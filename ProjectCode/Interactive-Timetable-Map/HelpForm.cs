7b7632908ad7d81f734e7c8ffbcf71e5e15061d5
KrinK1337 <KrinK1337@users.noreply.github.com>
2020-03-03 11:07:29 +0000

    Updated Sprint Log

--- ProjectCode/Interactive-Timetable-Map/HelpForm.cs
+++ ProjectCode/Interactive-Timetable-Map/HelpForm.cs
@@ -13,6 +13,7 @@ namespace Interactive_Timetable_Map
     public partial class HelpForm : Form
     {
         int helpIndex = 1;
+        int helpIndexUserMax = 8;
         public HelpForm()
         {
             InitializeComponent();
@@ -28,37 +29,37 @@ namespace Interactive_Timetable_Map
             {
                 case 1:
                     helpTextBox.Text = "Welcome to the CCCU Interactive Timetable Map!";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 2:
                     helpTextBox.Text = "To get started, you will need to first log in with your universities login details.";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 3:
                     helpTextBox.Text = "Click the 'Login' button in the top right and enter your username and password!";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 4:
                     helpTextBox.Text = "Once you have logged in successfully, your timetable will be shown in full.";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 5:
                     helpTextBox.Text = "You can view your timetabled sessions, see how long they last for, which room you will be in and who is your lecturer.";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 6:
                     helpTextBox.Text = "If you don't recognise the room name or don't know where it is located,"
                                         + Environment.NewLine + 
                                         "just click on the room name and you will be directed to the location of the room.";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 7:
                     helpTextBox.Text = "Once you are done using this program, you may either close it, or log out.";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
                 case 8:
                     helpTextBox.Text = "Enjoy your time at CCCU!";
-                    helpIndexTextBox.Text = helpIndex + " / 8";
+                    helpIndexTextBox.Text = helpIndex + " / " + helpIndexUserMax;
                     break;
             }
         }
